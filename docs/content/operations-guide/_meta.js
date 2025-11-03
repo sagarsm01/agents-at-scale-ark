d4fe5d89e94859dd1b996942779d3d47c28af8da
sagar_mck <sagarmoy_sengupta@mckinsey.com>
2025-11-03 11:27:39 +0530

    docs: add airgap images builder documentation to operations guide

--- docs/content/operations-guide/_meta.js
+++ docs/content/operations-guide/_meta.js
@@ -2,6 +2,7 @@ export default {
   'provisioning': 'Cloud Infrastructure Provisioning',
   'build-pipelines': 'Build Pipelines',
   'deploying-ark': 'Deploying ARK',
+  'airgap-images': 'Building Airgapped Images',
   'penetration-testing-reports': 'Penetration Testing Reports',
   'code-analysis-reports': 'Code Analysis Reports',
   'artifact-analysis-reports': 'Artifact Analysis Reports',
